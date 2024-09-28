1933a85d174147fd4c0ed645d29be19edcb0fdb1
kafkaphoenix <jaguilerapuerta@gmail.com>
2024-09-29 00:32:21 +0200

    fix: move template definition to the header to avoid linking error, fix contains method [] breaking const correctness

--- src/newleaf/assets/assets_manager.h
+++ src/newleaf/assets/assets_manager.h
@@ -6,6 +6,8 @@
 #include <string_view>
 #include <unordered_map>
 
+#include "../logging/log_manager.h"
+#include "../utils/assert.h"
 #include "../utils/numeric_comparator.h"
 #include "asset.h"
 
@@ -13,15 +15,50 @@ namespace nl {
 class AssetsManager {
   public:
     template <typename Type, typename... Args>
-    void load(std::string_view id, Args&&... args);
+    void load(std::string_view id, Args&&... args) {
+      std::string_view type = typeid(Type).name();
+      type = type.substr(type.find_last_of(':') + 1);
+      auto& asset_map = m_assets[type.data()];
+      ENGINE_ASSERT(not asset_map.contains(id.data()),
+                    "asset {} already exists for type {}!", id, type);
+      asset_map.emplace(id,
+                        std::make_shared<Type>(std::forward<Args>(args)...));
+      m_dirty = true;
+    }
 
-    template <typename Type> bool contains(std::string_view id) const;
+    template <typename Type> bool contains(std::string_view id) const {
+      std::string_view type = typeid(Type).name();
+      type = type.substr(type.find_last_of(':') + 1);
+      auto& asset_map = m_assets.at(type.data());
+      return asset_map.contains(id.data());
+    }
 
     template <typename Type>
-    std::shared_ptr<Type> get(std::string_view id) const;
+    std::shared_ptr<Type> get(std::string_view id) const {
+      std::string_view type = typeid(Type).name();
+      type = type.substr(type.find_last_of(':') + 1);
+      ENGINE_ASSERT(contains<Type>(id), "asset {} not found for type {}!", id,
+                    type);
+      return std::static_pointer_cast<Type>(
+        m_assets.at(type.data()).at(id.data())); // I know the type is correct
+    }
 
     template <typename Type, typename... Args>
-    std::shared_ptr<Type> reload(std::string_view id, Args&&... args);
+    std::shared_ptr<Type> reload(std::string_view id, Args&&... args) {
+      std::string_view type = typeid(Type).name();
+      type = type.substr(type.find_last_of(':') + 1);
+      ENGINE_ASSERT(contains<Type>(id), "asset {} not found for type {}!", id,
+                    type);
+      auto& maybe_asset = m_assets.at(type.data()).at(id.data());
+      std::shared_ptr<Asset> asset =
+        std::make_shared<Type>(std::forward<Args>(args)...);
+      maybe_asset = std::move(asset);
+
+      m_dirty = true;
+      ENGINE_TRACE("reloaded asset {}", id);
+      return std::static_pointer_cast<Type>(
+        maybe_asset); // I know the type is correct
+    }
 
     void clear();
 
