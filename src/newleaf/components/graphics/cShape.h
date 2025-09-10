076e3c3e644cb4c1c301e2c0ee6923c3e06e805c
kafkaphoenix <jaguilerapuerta@gmail.com>
2025-09-10 20:12:06 +0200

    fix: shape repeat texture false by default

--- src/newleaf/components/graphics/cShape.h
+++ src/newleaf/components/graphics/cShape.h
@@ -49,7 +49,7 @@ struct CShape {
 
     std::string get_mesh_info(uint32_t index) const { return map_to_json(meshes.at(index).to_map()); }
 
-    // TODO repeat can't be modified, should be add here to control overflow repeat? texture repeat is defined in the prefab
+    // TODO repeat texture should be linked to each texture rethink for now always false
     void create_mesh() {
       ENGINE_ASSERT(size.x > 0.f and (size.y > 0.f or _type == "triangle"),
                     "shape witdh and height must be greater than 0");
@@ -61,10 +61,10 @@ struct CShape {
         mesh.vao = ShapeFactory::create_triangle(size.x);
       } else if (_type == "rectangle") {
         type = CShape::Type::rectangle;
-        mesh.vao = ShapeFactory::create_rectangle(size.x, size.y, true);
+        mesh.vao = ShapeFactory::create_rectangle(size.x, size.y, false);
       } else if (_type == "cube") {
         type = CShape::Type::cube;
-        mesh.vao = ShapeFactory::create_cube(size.x, size.y, size.z, true);
+        mesh.vao = ShapeFactory::create_cube(size.x, size.y, size.z, false);
       } else if (_type == "circle") {
         type = CShape::Type::circle;
         mesh.vao = ShapeFactory::create_circle(size.x, size.y);
