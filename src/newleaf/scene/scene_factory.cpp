d5b83b60e40c2521b014e7aaf6249ebfc91dd005
kafkaphoenix <jaguilerapuerta@gmail.com>
2025-09-12 19:34:13 +0200

    fix: metrics for entities

--- src/newleaf/scene/scene_factory.cpp
+++ src/newleaf/scene/scene_factory.cpp
@@ -183,16 +183,16 @@ std::map<std::string, std::string, NumericComparator>& SceneFactory::compute_met
   m_metrics.clear();
   m_metrics["active_scene"] = m_active_scene;
   int created = registry.storage<entt::entity>().size();
-  int released = registry.storage<entt::entity>().free_list();
-  int total = created - released;
+  int alive = registry.storage<entt::entity>().free_list();
+  int released = created - alive;
   int n_prototypes = 0;
   for (const auto& [key, prototypes] : m_entity_factory.get_all_prototypes()) {
     n_prototypes += prototypes.size();
     m_metrics["prototypes_alive_from_prefab_" + key] = std::to_string(prototypes.size());
   }
   m_metrics["prototypes_alive"] = std::to_string(n_prototypes);
-  m_metrics["instances_alive"] = std::to_string(total - n_prototypes);
-  m_metrics["entities_alive"] = std::to_string(total);
+  m_metrics["instances_alive"] = std::to_string(alive - n_prototypes);
+  m_metrics["entities_alive"] = std::to_string(alive);
   m_metrics["entities_created"] = std::to_string(created);
   m_metrics["entities_released"] = std::to_string(released);
   m_dirty_metrics = false;
